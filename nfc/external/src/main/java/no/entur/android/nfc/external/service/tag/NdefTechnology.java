7ce231f1e283045b22808a2ddb0c410254c08a49
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2022-05-10 22:14:09 +0200

    Fix interface, package name

--- nfc/external/src/main/java/no/entur/android/nfc/external/service/tag/NdefTechnology.java
+++ nfc/external/src/main/java/no/entur/android/nfc/external/service/tag/NdefTechnology.java
@@ -2,8 +2,6 @@ package no.entur.android.nfc.external.service.tag;
 
 import android.nfc.NdefMessage;
 
-import no.entur.android.nfc.wrapper.tech.TagTechnology;
-
 public interface NdefTechnology extends TagTechnology {
     
     int ndefMakeReadOnly();
