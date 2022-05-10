53f335f94eea7ddfc542725dc0e2df78e935952b
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2022-05-10 22:15:49 +0200

    Fix import

--- nfc/external/src/main/java/no/entur/android/nfc/external/service/tag/NdefFormattableTechnology.java
+++ nfc/external/src/main/java/no/entur/android/nfc/external/service/tag/NdefFormattableTechnology.java
@@ -1,7 +1,5 @@
 package no.entur.android.nfc.external.service.tag;
 
-import no.entur.android.nfc.wrapper.tech.TagTechnology;
-
 public interface NdefFormattableTechnology extends TagTechnology {
 
     int formatNdef();
