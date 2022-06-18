dc43238dba1f657016b72d9310ec12a0c67799a3
Simon Hafner <hafnersimon@gmail.com>
2022-06-18 02:25:48 +0200

    Fixed typo in intent name (#8)

--- nfc/external/src/main/java/no/entur/android/nfc/external/ExternalNfcTagCallback.java
+++ nfc/external/src/main/java/no/entur/android/nfc/external/ExternalNfcTagCallback.java
@@ -12,7 +12,7 @@ public interface ExternalNfcTagCallback extends ReaderCallback {
 	/** Action corresponding to {@linkplain android.nfc.NfcAdapter#ACTION_NDEF_DISCOVERED}. */
 	public static final String ACTION_NDEF_DISCOVERED = ExternalNfcTagCallback.class.getName() + ".action.NDEF_DISCOVERED";
 	/** Action corresponding to {@linkplain android.nfc.NfcAdapter#ACTION_TAG_DISCOVERED}. */
-	public static final String ACTION_TAG_DISCOVERED = ExternalNfcTagCallback.class.getName() + "action.TAG_DISCOVERED";
+	public static final String ACTION_TAG_DISCOVERED = ExternalNfcTagCallback.class.getName() + ".action.TAG_DISCOVERED";
 	/** Action corresponding to {@linkplain android.nfc.NfcAdapter#ACTION_TECH_DISCOVERED}. */
 	public static final String ACTION_TECH_DISCOVERED = ExternalNfcTagCallback.class.getName() + ".action.TECH_DISCOVERED";
 	/** Action corresponding to hidden {@linkplain android.nfc.NfcAdapter#ACTION_TAG_LEFT_FIELD}. */
