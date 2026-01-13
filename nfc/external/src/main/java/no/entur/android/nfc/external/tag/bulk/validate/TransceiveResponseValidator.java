b0ef23282ee1da850edc4b582616b271919c17bf
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2026-01-13 14:05:45 +0100

    Fix merge

--- nfc/external/src/main/java/no/entur/android/nfc/external/tag/bulk/validate/TransceiveResponseValidator.java
+++
@@ -1,9 +0,0 @@
-package no.entur.android.nfc.external.tag.bulk.validate;
-
-import android.os.Parcelable;
-
-public interface TransceiveResponseValidator extends Parcelable {
-
-    boolean isValid(byte[] response);
-
-}
